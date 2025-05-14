9179a1f0878bbba3af08d82216214ff2ee9729c3
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-14 13:50:25 +0300

    feat: add append_file function to append content to an existing .txt file

--- lib/file_io.py
+++ lib/file_io.py
@@ -3,7 +3,8 @@ def write_file(file_name, file_content):
         file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as file:
+        file.write("\n"  + append_content)
 
 def read_file(file_name):
     pass
