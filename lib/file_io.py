4df0857cd8ed70ce81ba18c8a8aa04473e46d67a
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-14 13:53:13 +0300

    fix: remove extra newline in append_file to match test expectations

--- lib/file_io.py
+++ lib/file_io.py
@@ -4,7 +4,7 @@ def write_file(file_name, file_content):
 
 def append_file(file_name, append_content):
     with open(f"{file_name}.txt", "a") as file:
-        file.write("\n"  + append_content)
+         file.write(append_content)
 
 def read_file(file_name):
     with open(f"{file_name}.txt", "r") as file:
